dc182cfbad12bc68f4cbe731ce01de8362520538
name <me@example.com>
2024-06-04 12:30:57 +0200

    bild wird angezeigt und Counter implementiert

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch('http://localhost:3000/api/image');
     const data = await response.json();
 
-    document.getElementById('pic').attributes("href", data.url);
+    document.getElementById('pic').setAttribute("href", data.url);
     
 
 });
 
 var counter = 0;
-function IncreaseCount() {
+function increaseCounter() {
     counter++;
     document.getElementById("counter").innerHTML = "Gefundene Fehler: " + counter;
 }
